7136201f82373b321ef5ecee34a170251feebdf4
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-09-29 14:14:52 +0300

    sounds2

--- speach.py
+++ speach.py
@@ -8,7 +8,9 @@ from gtts import gTTS
 from playsound import playsound
 
 # It is a text value that we want to convert to audio
-text_val = 'Адзін.'
+
+
+text_val = '0'
 
 # Here are converting in English Language
 language = 'en'
@@ -21,7 +23,7 @@ obj = gTTS(text=text_val, lang=language, slow=False)
 
 # Here we are saving the transformed audio in a mp3 file named
 # exam.mp3
-obj.save("exam.mp3")
+obj.save("zero.mp3")
 
 # Play the exam.mp3 file
-playsound("exam.mp3")
+# playsound("two.mp3")
